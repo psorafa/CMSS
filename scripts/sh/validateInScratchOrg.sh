551ece620162d0acfe6f804467765d9b845b9b2a
Brzák <tadeas.brzak@adwise.cz>
2020-03-04 13:33:25 +0100

    validation script test

--- scripts/sh/validateInScratchOrg.sh
+++ scripts/sh/validateInScratchOrg.sh
@@ -10,12 +10,15 @@ DAYS=${5-1}
 #cleanup when done
 function finish {
     # remove scratch org
+    echo -e "\e[33mDeleting scratch org..."
     sfdx force:org:delete --targetusername $ALIAS --noprompt
+    echo -e "\e[33mDone."
 }
 trap finish EXIT
 set -o xtrace
 
 #create scratch org
+echo -e "\e[33mCreating scratch org..."
 if [ -n  "$DEVHUB"];
 then
     sfdx force:org:create --setalias $ALIAS --durationdays $DAYS --definitionfile  $CONF
@@ -24,10 +27,12 @@ else
 fi
 
 #push source
+echo -e "\e[33mPushing source..."
 sfdx force:source:push --targetusername $ALIAS
 
 #run tests
+echo -e "\e[33mRunning tests..."
 sfdx force:apex:test:run --targetusername $ALIAS --testlevel $TEST --codecoverage --resultformat human
 
 set +o
-echo "Validation Successfull!"
+echo -e "\e[32mValidation Successfull!"
