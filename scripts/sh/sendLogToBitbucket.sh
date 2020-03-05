ecdd913ccb0a6f98f107d6c318e36a50355736f3
Brzák <tadeas.brzak@adwise.cz>
2020-03-05 13:26:23 +0100

    send log test

---
+++ scripts/sh/sendLogToBitbucket.sh
@@ -0,0 +1,34 @@
+#!C:\progra~1\Git\bin\sh.exe
+
+# arguments:
+
+# Bamboo user:
+#   1: name
+#   2: password
+# Bitbucket user:
+#   3: name
+#   4: password
+# Other:
+#   5: ${bamboo.buildKey}
+#   6: ${bamboo.buildResultKey}
+#   7: git project key
+#   8: git repo key
+#   9: git pull request key: ${bamboo.repository.pr.key}
+
+
+alias jq=C:/Users/virt7173/bamboo-home/tools/jq-win64.exe
+
+# download log:
+curl -X GET --user ${1}:${2} "http://localhost:8086/download/${5}/build_logs/${6}.log"
+
+attachmentUrl=$(curl -k -u ${3}:${4} \
+     -X POST 'https://localhost:8443/projects/${7}/repos/${8}/attachments' \
+     -H 'Content-Type: multipart/form-data;' \
+     -F 'files=@${6}.log' \
+     | jq '.attachments[0].url')
+
+curl -k -u ${3}:${4} \
+     -H 'Content-type: application/json' \
+     -X POST 'https://localhost:8443/rest/api/latest/projects/${7}/repos/${8}/pull-requests/${9}/comments' \
+     -d '{"text": "Log file: - [${6}.log](${attachmentUrl})"}'
+
