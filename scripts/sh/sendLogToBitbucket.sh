3a788ca4b80845ce5957f7849c79934fdfd57fe4
Brzák <tadeas.brzak@adwise.cz>
2020-03-05 13:58:17 +0100

    send log test

--- scripts/sh/sendLogToBitbucket.sh
+++ scripts/sh/sendLogToBitbucket.sh
@@ -15,20 +15,19 @@
 #   8: git repo key
 #   9: git pull request key: ${bamboo.repository.pr.key}
 
-
+shopt -s expand_aliases
 alias jq=C:/Users/virt7173/bamboo-home/tools/jq-win64.exe
 
 # download log:
-curl -X GET --user ${1}:${2} "http://localhost:8086/download/${5}/build_logs/${6}.log"
+curl -X GET --user ${1}:${2} "http://gitvyvoj.cmss.local:8086/download/${5}/build_logs/${6}.log" -o SCRM-SFCI3-JOB1-3.log
 
-attachmentUrl=$(curl -k -u ${3}:${4} \
-     -X POST 'https://localhost:8443/projects/${7}/repos/${8}/attachments' \
+attachmentUrl=$(curl -k -u ${3}:${4} --insecure \
+     -X POST 'https://gitvyvoj.cmss.local:8443/projects/${7}/repos/${8}/attachments' \
      -H 'Content-Type: multipart/form-data;' \
      -F 'files=@${6}.log' \
-     | jq '.attachments[0].url')
+     | jq -j '.attachments[0].url')
 
 curl -k -u ${3}:${4} \
      -H 'Content-type: application/json' \
-     -X POST 'https://localhost:8443/rest/api/latest/projects/${7}/repos/${8}/pull-requests/${9}/comments' \
-     -d '{"text": "Log file: - [${6}.log](${attachmentUrl})"}'
-
+     -X POST 'https://gitvyvoj.cmss.local:8443/rest/api/latest/projects/${7}/repos/${8}/pull-requests/${9}/comments' \
+     -d '{"text": "Log file for last build: [${6}.log](${attachmentUrl})"}'
