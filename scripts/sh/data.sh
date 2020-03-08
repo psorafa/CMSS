c4ed71c60cc80f85af324bf99dfb5bf8ffbfa55e
packocz <ales.remta@adwise.cz>
2020-03-08 21:59:43 +0100

    extend test data script with opps, product and assets

--- scripts/sh/data.sh
+++ scripts/sh/data.sh
@@ -8,8 +8,28 @@ mkdir scripts/apex/tmp -p
 accountsScript="scripts/apex/tmp/importAccounts.apex"
 scripts/sh/data/Accounts.sh "$accountsScript"
 
+productScript="scripts/apex/tmp/importProducts.apex"
+scripts/sh/data/Products.sh "$productScript"
+
+opportunityScript="scripts/apex/tmp/importOpportunities.apex"
+scripts/sh/data/Opportunities.sh "$opportunityScript"
+
+assetScript="scripts/apex/tmp/importAssets.apex"
+scripts/sh/data/Assets.sh "$assetScript"
+
 set -o xtrace
-sfdx force:apex:execute --apexcodefile $accountsScript --targetusername $ALIAS
+if [ -n  "$ALIAS"];
+then
+    sfdx force:apex:execute --apexcodefile $accountsScript
+    sfdx force:apex:execute --apexcodefile $productScript
+    sfdx force:apex:execute --apexcodefile $opportunityScript
+    sfdx force:apex:execute --apexcodefile $assetScript
+else
+    sfdx force:apex:execute --apexcodefile $accountsScript --targetusername $ALIAS
+    sfdx force:apex:execute --apexcodefile $productScript --targetusername $ALIAS
+    sfdx force:apex:execute --apexcodefile $opportunityScript --targetusername $ALIAS
+    sfdx force:apex:execute --apexcodefile $assetScript --targetusername $ALIAS
+fi
 
 rm -r scripts/apex/tmp
 
