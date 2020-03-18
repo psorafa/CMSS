7aa41e884e91b4f620d385941d82adcade9efc5a
packocz <ales.remta@adwise.cz>
2020-03-18 16:25:45 +0100

    fix param names

--- scripts/sh/bamboo/util/gitDiffJoinToLine.sh
+++ scripts/sh/bamboo/util/gitDiffJoinToLine.sh
@@ -7,11 +7,11 @@
 #   3: git diff filter - required
 #   4: subfolder to compare - require
 
-SOURCE_BRANCH=${1}
-CURRENT_BRANCH=${2}
+SOURCE_COMMIT=${1}
+CURRENT_COMMIT=${2}
 FILTER=${3}
 FOLDER=${4}
 
-my_array=( $(scripts/sh/bamboo/util/gitDiff.sh "$CURRENT_COMMIT" "$SOURCE_COMMIT" "$FILTER" "$FOLDER") )
+my_array=( $(scripts/sh/bamboo/util/gitDiff.sh "$CURRENT_COMMIT" "$SOURCE_COMMIT" "$FILTER" $FOLDER) )
 my_line=$(printf '%s,' "${my_array[@]}")
 echo ${my_line::-1}
