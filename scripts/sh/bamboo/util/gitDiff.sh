c990ca6eaedb44bfaaa60d6e1da1a92f0cfaa958
packocz <ales.remta@adwise.cz>
2020-03-18 16:14:26 +0100

    Correct order of params

--- scripts/sh/bamboo/util/gitDiff.sh
+++ scripts/sh/bamboo/util/gitDiff.sh
@@ -13,7 +13,7 @@ FILTER=${3}
 FOLDER=${4}
 
 IFS=$'\n'
-ORIGINAL_FILES=$(git diff -w --ignore-blank-lines --name-only --diff-filter="${FILTER}" "${CURRENT_BRANCH}" "${SOURCE_BRANCH}" ${FOLDER} | sed s/\"//g)
+ORIGINAL_FILES=$(git diff -w --ignore-blank-lines --name-only --diff-filter="${FILTER}" "${CURRENT_BRANCH}" "${SOURCE_BRANCH}" "${FOLDER}" | sed s/\"//g)
 
 for FILE in $ORIGINAL_FILES; do
     echo "${FILE}"
