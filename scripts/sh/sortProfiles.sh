44c2ae6c207ed0332e973d07b629f8c70819cc44
Brzák <tadeas.brzak@adwise.cz>
2020-03-30 19:04:01 +0200

    sort profiles script; sorted admin profile

---
+++ scripts/sh/sortProfiles.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Use this script to sort and deduplicate profiles before you create pul request.
+
+java -jar ../java/profilesSort.jar --directory.profiles="../../cmss/main/default/profiles"
