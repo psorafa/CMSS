6dd78af9c4b51c398a5e9ef5dbde1d5c36d66bfc
Brzák <tadeas.brzak@adwise.cz>
2020-03-30 20:42:01 +0200

    data model fixes, profile before conflict resolution

--- scripts/sh/sortProfiles.sh
+++ scripts/sh/sortProfiles.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Use this script to sort and deduplicate profiles before you create pull request.
 
-java -jar ../java/profilesSort.jar --directory.profiles="../../cmss/main/default/profiles"
+java -jar scripts/java/profilesSort.jar --directory.profiles="cmss/main/default/profiles"
