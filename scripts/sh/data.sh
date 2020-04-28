785a2fc652f22282c133ed3b0f1c3988d40a5e23
packocz <34923415+packocz@users.noreply.github.com>
2020-04-28 11:00:30 +0200

    Opportunities import

--- scripts/sh/data.sh
+++ scripts/sh/data.sh
@@ -17,7 +17,8 @@ scripts/sh/util/mergeJson.sh "$TARGETPATH/importAccounts.apex" "data/tree/person
 
 scripts/sh/util/mergeJson.sh "scripts/apex/importProducts.apex" "data/tree/products.json" "{0}" "$TARGETPATH/importProducts.apex"
 
-#scripts/sh/util/mergeJson.sh "scripts/apex/importOpportunities.apex" "data/tree/opportunities.json" "{0}" "$TARGETPATH/importOpportunities.apex"
+scripts/sh/util/mergeJson.sh "scripts/apex/importOpportunities.apex" "data/tree/opportunities.json" "{0}" "$TARGETPATH/importOpportunities.apex"
+scripts/sh/util/mergeJson.sh "scripts/apex/importOpportunities.apex" "data/tree/opportunities2.json" "{0}" "$TARGETPATH/importOpportunities2.apex"
 
 scripts/sh/util/mergeJson.sh "scripts/apex/importAssets.apex" "data/tree/assets.json" "{0}" "$TARGETPATH/importAssets.apex"
 
@@ -29,14 +30,16 @@ if [ -z "$ALIAS" ];
 then
     sfdx force:apex:execute --apexcodefile "$TARGETPATH/importAccounts.apex"
     sfdx force:apex:execute --apexcodefile "$TARGETPATH/importProducts.apex"
-    #sfdx force:apex:execute --apexcodefile "$TARGETPATH/importOpportunities.apex"
+    sfdx force:apex:execute --apexcodefile "$TARGETPATH/importOpportunities.apex"
+    sfdx force:apex:execute --apexcodefile "$TARGETPATH/importOpportunities2.apex"
     sfdx force:apex:execute --apexcodefile "$TARGETPATH/importAssets.apex"
     sfdx force:apex:execute --apexcodefile "$TARGETPATH/importTasks.apex"
     sfdx force:apex:execute --apexcodefile "$TARGETPATH/importTasks2.apex"
 else
     sfdx force:apex:execute --apexcodefile "$TARGETPATH/importAccounts.apex" --targetusername $ALIAS
     sfdx force:apex:execute --apexcodefile "$TARGETPATH/importProducts.apex" --targetusername $ALIAS
-    #sfdx force:apex:execute --apexcodefile "$TARGETPATH/importOpportunities.apex" --targetusername $ALIAS
+    sfdx force:apex:execute --apexcodefile "$TARGETPATH/importOpportunities.apex" --targetusername $ALIAS
+    sfdx force:apex:execute --apexcodefile "$TARGETPATH/importOpportunities2.apex" --targetusername $ALIAS
     sfdx force:apex:execute --apexcodefile "$TARGETPATH/importAssets.apex" --targetusername $ALIAS
     sfdx force:apex:execute --apexcodefile "$TARGETPATH/importTasks.apex" --targetusername $ALIAS
     sfdx force:apex:execute --apexcodefile "$TARGETPATH/importTasks2.apex" --targetusername $ALIAS
