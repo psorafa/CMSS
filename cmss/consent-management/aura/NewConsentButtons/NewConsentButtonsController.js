c256daa2155923fef8e0af33ad3bcfd298348bc7
Brzák <tadeas.brzak@adwise.cz>
2021-02-04 21:55:30 +0100

    new consents - aura component & flows refactoring

---
+++ cmss/consent-management/aura/NewConsentButtons/NewConsentButtonsController.js
@@ -0,0 +1,17 @@
+({
+    gotoNewGeneralConsentFlow : function (component, event, helper) {
+        var urlEvent = $A.get("e.force:navigateToURL");
+        urlEvent.setParams({
+          "url": "/flow/GeneralConsentCreation?accountId=" + component.get("v.recordId")
+        });
+        urlEvent.fire();
+    },
+
+    gotoNewInternalConsentFlow : function (component, event, helper) {
+        var urlEvent = $A.get("e.force:navigateToURL");
+        urlEvent.setParams({
+          "url": "/flow/InternalConsentCreation?accountId=" + component.get("v.recordId")
+        });
+        urlEvent.fire();
+    }
+});
