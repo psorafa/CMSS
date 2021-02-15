9945aca824a45e47a2b8161a705d98e0ff95832f
Brzák <tadeas.brzak@adwise.cz>
2021-02-15 20:42:21 +0100

    consents flow fixes & redirect back to account after flow finishes

--- cmss/consent-management/aura/NewConsentButtons/NewConsentButtonsController.js
+++ cmss/consent-management/aura/NewConsentButtons/NewConsentButtonsController.js
@@ -2,7 +2,7 @@
     gotoNewGeneralConsentFlow : function (component, event, helper) {
         var urlEvent = $A.get("e.force:navigateToURL");
         urlEvent.setParams({
-          "url": "/flow/GeneralConsentCreation?accountId=" + component.get("v.recordId")
+          "url": "/flow/GeneralConsentCreation?accountId=" + component.get("v.recordId") + "&retURL=%2F" + component.get("v.recordId")
         });
         urlEvent.fire();
     },
@@ -10,7 +10,7 @@
     gotoNewInternalConsentFlow : function (component, event, helper) {
         var urlEvent = $A.get("e.force:navigateToURL");
         urlEvent.setParams({
-          "url": "/flow/InternalConsentCreation?accountId=" + component.get("v.recordId")
+          "url": "/flow/InternalConsentCreation?accountId=" + component.get("v.recordId") + "&retURL=%2F" + component.get("v.recordId")
         });
         urlEvent.fire();
     }
