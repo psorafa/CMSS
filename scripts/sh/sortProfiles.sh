e3a5a57a4ed24235f0cdac9c1823ef7307f30ea7
Brzák <tadeas.brzak@adwise.cz>
2021-05-21 14:41:17 +0200

    fixed GenericRelatedList compoennt visibility issue, fixed some scripts

--- scripts/sh/sortProfiles.sh
+++ scripts/sh/sortProfiles.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Use this script to sort and deduplicate profiles before you create pull request.
 
-java -jar scripts/java/profilesSort.jar --directory.profiles="cmss/app/profiles"
+java -jar scripts/java/profilesSort.jar --directory.profiles="cmss/app/default/profiles"
