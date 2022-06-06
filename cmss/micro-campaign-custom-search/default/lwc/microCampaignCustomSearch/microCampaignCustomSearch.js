61a1142f25c66a34502b823431d1afe36f75ae75
Petr Vích <vichpetr@gmail.com>
2022-06-06 21:35:53 +0200

    create a count before the search

--- cmss/micro-campaign-custom-search/default/lwc/microCampaignCustomSearch/microCampaignCustomSearch.js
+++ cmss/micro-campaign-custom-search/default/lwc/microCampaignCustomSearch/microCampaignCustomSearch.js
@@ -1,5 +1,6 @@
 import { LightningElement, wire } from 'lwc';
 import searchResults from '@salesforce/apex/CustomSearchController.searchResults';
+import countResults from '@salesforce/apex/CustomSearchController.countResults';
 import loadFieldsetDetail from '@salesforce/apex/CustomSearchController.loadFieldsetDetail';
 import loadTypeList from '@salesforce/apex/CustomSearchController.loadTypeList';
 import createMicroCampaign from '@salesforce/apex/CustomSearchController.createMicroCampaign';
@@ -26,6 +27,7 @@ import LBL_UNSUPPORTED_OBJECT_TYPE_ERROR_MESSAGE from '@salesforce/label/c.Unsup
 import LBL_RECORDS_CREATED_MESSAGE from '@salesforce/label/c.RecordSuccessfullyCreated';
 import LBL_LOAD_PRODUCT_TYPES_ERROR_MESSAGE from '@salesforce/label/c.LoadingProductTypesError';
 import LBL_NO_RECORDS from '@salesforce/label/c.NoRecordsFound';
+import LBL_MAX_RECORDS_EXCEEDED from '@salesforce/label/c.MaxRecordsExceeded';
 
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 
@@ -192,38 +194,46 @@ export default class MicroCampaignCustomSearch extends LightningElement {
 
 		console.log('request: ' + JSON.stringify(request));
 		const currentSelectedAccountIds = this.selectedAccountIds;
+		countResults({ dto: request })
+			.then(countResult => {
+				this.totalRecordsCount = countResult;
+				const maxAllowedRecordsCount = 10000;
+				if (countResult <= maxAllowedRecordsCount) {
+					searchResults({ dto: request })
+						.then(response => {
+							this.outputTableData = response.data;
+							this.selectedAccountIds = currentSelectedAccountIds;
+							this.totalRecordsCount = response.totalCount;
 
-		searchResults({ dto: request })
-			.then(response => {
-				this.outputTableData = response.data;
-				this.selectedAccountIds = currentSelectedAccountIds;
-				this.totalRecordsCount = response.totalCount;
+							if (this.totalRecordsCount < 1) {
+								this.toastMessage(null, '', LBL_NO_RECORDS);
+								return;
+							}
 
-				if (this.totalRecordsCount < 1) {
-					this.toastMessage(null, '', LBL_NO_RECORDS);
-					return;
+							loadFieldsetDetail({
+								fieldsetName: this.selectedConfiguration.FieldsetName__c,
+								objectName: request.objectName
+							})
+								.then(colResponse => {
+									this.outputTableColumns = colResponse;
+									if (this.isTableVisible) {
+										this.section = ['data'];
+									} else {
+										this.section = ['configuration'];
+									}
+								})
+								.catch(error => {
+									console.log(JSON.stringify(error));
+									this.errorToastMessage('', error.body.message);
+								});
+						})
+						.catch(error => {
+							console.log(JSON.stringify(error));
+							this.errorToastMessage('', error.body.message);
+						});
+				} else {
+					this.errorToastMessage('', LBL_MAX_RECORDS_EXCEEDED + ' ' + maxAllowedRecordsCount);
 				}
-
-				loadFieldsetDetail({
-					fieldsetName: this.selectedConfiguration.FieldsetName__c,
-					objectName: request.objectName
-				})
-					.then(colResponse => {
-						this.outputTableColumns = colResponse;
-						if (this.isTableVisible) {
-							this.section = ['data'];
-						} else {
-							this.section = ['configuration'];
-						}
-					})
-					.catch(error => {
-						console.log(JSON.stringify(error));
-						this.errorToastMessage('', error.body.message);
-					});
-			})
-			.catch(error => {
-				console.log(JSON.stringify(error));
-				this.errorToastMessage('', error.body.message);
 			})
 			.finally(() => {
 				this.loading = false;
