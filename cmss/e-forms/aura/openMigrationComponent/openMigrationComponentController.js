9fe3adf8b268543550d81d8c7ec95bcf755d0753
PJ <petr.jankovsky@cgi.com>
2022-06-17 18:01:48 +0200

    eforms community changes

--- cmss/e-forms/aura/openMigrationComponent/openMigrationComponentController.js
+++ cmss/e-forms/aura/openMigrationComponent/openMigrationComponentController.js
@@ -3,32 +3,28 @@
  */
 
 ({
-    doInit : function(component) {
+    doInit: function(component) {
         console.log('som tu: ', component.get("v.recordId"));
-        var navService = component.find("navService");
-        //var workspaceAPI = component.find("workspace");
-        var pageReference = {
-            type: 'standard__component',
-            attributes: {
-                componentName: 'c__migrationPremiumWrapper',
-            },
-            state: {
-                "c__recId": component.get("v.recordId"),
-                "c__migrationLabel": 'E formulář'
+        var action = component.get("c.isPortalEnabled");
+        action.setCallback(this, function(response) {
+            if (response.getReturnValue()) {
+                var navurl = 'https://eforms-csobstavebni.cs108.force.com/s/eforms?c__migrationLabel=Eformular&c__recId=' + component.get("v.recordId");
+                window.location = navurl;
+            } else {
+                var navService = component.find("navService");
+                var pageReference = {
+                    type: 'standard__component',
+                    attributes: {
+                        componentName: 'c__migrationPremiumWrapper',
+                    },
+                    state: {
+                        "c__recId": component.get("v.recordId"),
+                        "c__migrationLabel": 'E formulář'
+                    }
+                };
+                navService.navigate(pageReference);
             }
-        };
-        navService.navigate(pageReference);
-        //dont know why getFocusedTabInfo works but getEnclosingTabId dont :( but it works
-        /*workspaceAPI.getFocusedTabInfo().then(function(enclosingTabId) {
-            workspaceAPI.openSubtab({
-                parentTabId: enclosingTabId.tabId,
-                pageReference: pageReference
-            }).then(function(subtabId) {
-                console.log("The new subtab ID is:" + subtabId);
-            }).catch(function(error) {
-                console.error(error);
-            });
-        });*/
+        });
+        $A.enqueueAction(action);
     }
-
 });
--- cmss/e-forms/lwc/pcfSignpost/pcfSignpost.js
+++ cmss/e-forms/lwc/pcfSignpost/pcfSignpost.js
@@ -2,13 +2,14 @@
  * Created by lukas.krbec on 03.06.2022.
  */
 import { LightningElement, api } from 'lwc';
-import {NavigationMixin} from "lightning/navigation";
-import {ShowToastEvent} from "lightning/platformShowToastEvent";
+import { NavigationMixin } from "lightning/navigation";
+import { ShowToastEvent } from "lightning/platformShowToastEvent";
 import quickActionStyle from "@salesforce/resourceUrl/customQuickActionStyle";
 import { loadStyle } from "lightning/platformResourceLoader";
 import { CloseActionScreenEvent } from "lightning/actions";
 
 import getRelatedMigrationProcesses from '@salesforce/apex/PcfSignpostController.getRelatedMigrationProcesses';
+import isPortalEnabled from '@salesforce/apex/PcfSignpostController.isPortalEnabled';
 
 import Header from '@salesforce/label/c.SignpostHeader';
 import MissingProcessesLabel from '@salesforce/label/c.MissingProcessesLabel';
@@ -16,69 +17,79 @@ import MissingProcessesText from '@salesforce/label/c.MissingProcessesText';
 
 
 export default class pcfSignpost extends NavigationMixin(LightningElement) {
-        @api recId;
-        loaded;
-        processes;
-        labels = {
-            missingProcessesLabel : MissingProcessesLabel,
-            missingProcessesText : MissingProcessesText,
-            header : Header,
-        };
+    @api recId;
+    loaded;
+    processes;
+    labels = {
+        missingProcessesLabel: MissingProcessesLabel,
+        missingProcessesText: MissingProcessesText,
+        header: Header,
+    };
 
-        toastVariant = {
-                    ERR: 'error',
-                    WAR: 'warning',
-        };
+    toastVariant = {
+        ERR: 'error',
+        WAR: 'warning',
+    };
 
-        connectedCallback() {
-            loadStyle(this, quickActionStyle);
-            console.log('connect');
-            getRelatedMigrationProcesses({recordId: this.recId})
-                .then( data => {
-                    this.processes = data;
-                    if(data.length < 1){
-                        this.showToast(this.labels.missingProcessesLabel,this.labels.missingProcessesText,this.toastVariant.WAR);
-                    }
-                    this.loaded = true;
-                }).catch(error => {
-                    console.log(error);
-                    this.showToast(error.body.message,error.body.stackTrace,this.toastVariant.ERR);
-                }).finally(() => {
-                });
-        }
-
-
-
-        handleChooseMigration(evt){
-            const name = evt.currentTarget.name;
-            let recId = `${this.recId}`;
-            let migrationProcess = name;
-            let migrationLabel = evt.currentTarget.label;
-            let label = name;
-            this.navigateToMigrationWrapper(recId,migrationProcess,label,migrationLabel);
-        }
-
-        navigateToMigrationWrapper(recId,migrationProcess,errorLabel,migrationLabel) {
-            this[NavigationMixin.Navigate]({
-                type: 'standard__component',
-                attributes: {
-                    componentName: 'c__migrationPremiumWrapper'
-                },
-                state: {
-                    c__migrationProcess : migrationProcess,
-                    c__migrationLabel : migrationLabel,
-                    c__recId : recId,
+    connectedCallback() {
+        loadStyle(this, quickActionStyle);
+        console.log('connect');
+        getRelatedMigrationProcesses({ recordId: this.recId })
+            .then(data => {
+                this.processes = data;
+                if (data.length < 1) {
+                    this.showToast(this.labels.missingProcessesLabel, this.labels.missingProcessesText, this.toastVariant.WAR);
                 }
-            });
-        }
+                this.loaded = true;
+            }).catch(error => {
+                console.log(error);
+                this.showToast(error.body.message, error.body.stackTrace, this.toastVariant.ERR);
+            }).finally(() => {});
+    }
 
-        showToast(title, message, variant) {
-                const event = new ShowToastEvent({
-                    title: title,
-                    variant: variant,
-                    message: message,
-                    mode:'sticky',
-                });
-                this.dispatchEvent(event);
-            }
+
+
+    handleChooseMigration(evt) {
+        const name = evt.currentTarget.name;
+        let recId = `${this.recId}`;
+        let migrationProcess = name;
+        let migrationLabel = evt.currentTarget.label;
+        let label = name;
+        this.navigateToMigrationWrapper(recId, migrationProcess, label, migrationLabel);
+    }
+
+    navigateToMigrationWrapper(recId, migrationProcess, errorLabel, migrationLabel) {
+        isPortalEnabled()
+            .then(data => {
+                if (data) {
+                    var navurl = 'https://eforms-csobstavebni.cs108.force.com/s/eforms?c__migrationProcess=' + migrationProcess + '&c__migrationLabel=Eformular&c__recId=' + recId;
+                    window.location = navurl;
+                } else {
+                    this[NavigationMixin.Navigate]({
+                        type: 'standard__component',
+                        attributes: {
+                            componentName: 'c__migrationPremiumWrapper'
+                        },
+                        state: {
+                            c__migrationProcess: migrationProcess,
+                            c__migrationLabel: migrationLabel,
+                            c__recId: recId,
+                        }
+                    });
+                }
+            }).catch(error => {
+                console.log(error);
+                this.showToast(error.body.message, error.body.stackTrace, this.toastVariant.ERR);
+            }).finally(() => {});
+    }
+
+    showToast(title, message, variant) {
+        const event = new ShowToastEvent({
+            title: title,
+            variant: variant,
+            message: message,
+            mode: 'sticky',
+        });
+        this.dispatchEvent(event);
+    }
 }
