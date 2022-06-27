30858b90a55008ee5e6f3c56f767cfc191330cdf
Linda Langerová <linda.langerova@enehano.cz>
2022-06-27 17:16:51 +0200

    46365 Remove possibility to change assignee for community users

--- cmss/main/common-components/micro-campaigns/lwc/selectAssignee/selectAssignee.js
+++ cmss/main/common-components/micro-campaigns/lwc/selectAssignee/selectAssignee.js
@@ -1,28 +1,43 @@
-import { LightningElement, api, track } from 'lwc';
+import { LightningElement, wire, track } from 'lwc';
+
+import { getRecord } from 'lightning/uiRecordApi';
+import USER_ID from '@salesforce/user/Id';
+import USERPROFILE_NAME from '@salesforce/schema/User.Profile.Name';
 
 export default class SelectAssignee extends LightningElement {
+	@track _options = [
+		{ label: 'Správce stavu', value: 'portfolioManager' },
+		{ label: 'Vybrat uživatele...', value: 'selectUser' }
+	];
+	@track _selection = 'portfolioManager';
 
-    @track _options = [
-        { label: 'Správce stavu', value: 'portfolioManager' },
-        { label: 'Vybrat uživatele...', value: 'selectUser' }
-    ]
-    @track _selection = 'portfolioManager'
+	isPartnerUser = false;
 
-    get displaySearch() {
-        return this._selection === 'selectUser'
-    }
+	get displaySearch() {
+		return this._selection === 'selectUser';
+	}
 
-    handleSelectionChange(event) {
-        event.stopPropagation()
-        this._selection = event.detail.value
-        if (this._selection === 'portfolioManager') {
-            this.dispatchEvent(new CustomEvent('change', { detail: undefined } ) )
-        }
-    }
+	@wire(getRecord, { recordId: USER_ID, fields: [USERPROFILE_NAME] })
+	userDetails({ error, data }) {
+		if (data) {
+			if (data.fields.Profile.value.fields.Name.value == 'CMSS Experience User') {
+				this.isPartnerUser = true;
+			}
+		} else if (error) {
+			this.error = error;
+		}
+	}
 
-    handleLookupChange(event) {
-        event.stopPropagation()
-        this.dispatchEvent(new CustomEvent('change', { detail: event.detail } ) )
-    }
+	handleSelectionChange(event) {
+		event.stopPropagation();
+		this._selection = event.detail.value;
+		if (this._selection === 'portfolioManager') {
+			this.dispatchEvent(new CustomEvent('change', { detail: undefined }));
+		}
+	}
 
+	handleLookupChange(event) {
+		event.stopPropagation();
+		this.dispatchEvent(new CustomEvent('change', { detail: event.detail }));
+	}
 }
