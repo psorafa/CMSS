9467699ece1f05d74b64322d5767ce5b4ebf7998
Linda Langerová <linda.langerova@enehano.cz>
2022-12-09 16:05:55 +0100

    Get sf-metadata-utils permissions optimizer from Enehano github

---
+++ scripts/ps/sortProfiles.ps1
@@ -0,0 +1,4 @@
+# 1: Run script that sorts profiles and permission sets
+# Use this script to sort and deduplicate profiles before you create pull request.
+# Select path "cmss"
+Invoke-Expression "node ./scripts/js/sf-metadata-utils/dist/lib.js"
