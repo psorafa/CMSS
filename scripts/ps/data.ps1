01f6c73798f73494f37802e976f0394215de4986
Tadeáš Brzák <tadeas.brzak@enehano.cz>
2022-12-15 14:50:31 +0100

    open org after successful creation old data scripts cleanup fixed core-data export/import, improved scripts improved create scratch org script, now supporting data load

--- scripts/ps/data.ps1
+++ scripts/ps/data.ps1
@@ -1,4 +1,39 @@
 
+$ALIAS=$args[0]
+if ($null -eq $ALIAS) {
+    Write-Host "Alias argument is required." -ForegroundColor Red
+    exit
+}
 
-Write-Host "Warning: Data insertion not supported yet." -ForegroundColor Yellow
-Write-Host "If you want to create data, run './scripts/sh/data.sh' in bash terminal." -ForegroundColor Yellow
+$ErrorActionPreference = "Stop"
+function CheckLastExitCode {
+    param ([int[]]$SuccessCodes = @(0))
+    if (!$?) {
+        Write-Host "Last CMD failed" -ForegroundColor Red
+        #GoToWrapperDirectory in my code I go back to the original directory that launched the script
+        exit
+    }
+    if ($SuccessCodes -notcontains $LastExitCode) {
+        Write-Host "EXE RETURNED EXIT CODE $LastExitCode" -ForegroundColor Red
+        #GoToWrapperDirectory in my code I go back to the original directory that launched the script
+        exit
+    }
+}
+try {
+    if (!(sfdx plugins |Select-String -Pattern "sfdmu")) {
+        Write-Host "Plugin not installed: sfdmu" -ForegroundColor Yellow
+        Write-Host "Trying to install..." -ForegroundColor Yellow
+        sfdx plugins:install sfdmu
+    }
+
+    sfdx sfdmu:run -p data/core-data --sourceusername csvfile --targetusername $ALIAS
+    CheckLastExitCode
+    sfdx sfdmu:run -p data/custom-search --sourceusername csvfile --targetusername $ALIAS
+    CheckLastExitCode
+
+    Write-Host "Data loading completed." -ForegroundColor Green
+} finally {
+    # cleanup when done
+    git checkout -- data/core-data/target
+    git checkout -- data/custom-search/target
+}
